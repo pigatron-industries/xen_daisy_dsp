5b158f5b0aab3e7893b39101fb22ecf6bfcf3cbd
Robert Ellis <rob@pigatron-industries.co.uk>
2020-08-16 16:34:08 +0100

    Made simple filter work

--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,16 @@ size_t num_channels;
 Svf filterLeft;
 Svf filterRight;
 
+float frequency;
+
 #define LEFT 0
 #define RIGHT 1
 
 void AudioCallback(float **in, float **out, size_t size)
 {
+    filterLeft.SetFreq(frequency);
+    filterRight.SetFreq(frequency);
+
     for (size_t i = 0; i < size; i++)
     {
         filterLeft.Process(in[LEFT][i]);
@@ -23,6 +28,9 @@ void AudioCallback(float **in, float **out, size_t size)
 }
 
 void setup() {
+    Serial.begin(115200);
+    Serial.println("Pigatron Industries");
+
     float sample_rate;
     // Initialize for Daisy pod at 48kHz
     hardware = DAISY.init(DAISY_SEED, AUDIO_SR_48K);
@@ -32,8 +40,8 @@ void setup() {
     // Initialize Filter, and set parameters.
     filterLeft.Init(sample_rate);
     filterLeft.SetFreq(500.0);
-    filterLeft.SetRes(0.85);
-    filterLeft.SetDrive(0.8);
+    filterLeft.SetRes(1);
+    filterLeft.SetDrive(1);
 
     filterRight.Init(sample_rate);
     filterRight.SetFreq(500.0);
@@ -44,4 +52,6 @@ void setup() {
 }
 
 void loop() {
+    float frequencyCV = ((analogRead(A0) / 1023.0) * -10.0) + 10;
+    frequency = 27.5*powf(2, frequencyCV);
 }

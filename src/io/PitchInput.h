b6b050b2fd7c2be40c6fbf225290a92153f0dc01
Robert Ellis <rob@pigatron-industries.co.uk>
2020-09-28 14:12:20 +0100

    Added zero frequency

--- src/io/PitchInput.h
+++ src/io/PitchInput.h
@@ -12,11 +12,15 @@ class PitchInput {
         inline void update() {
             value = analogRead(pin);
             voltage = ((value / 1023.0) * -10.0) + 5;
-            frequency = 880*powf(2, voltage);
+            frequency = zeroFrequency*powf(2, voltage);
         }
 
         inline float getFrequency() {
             return frequency;
+        }
+
+        inline void setZeroFrequency(float _zeroFrequency) {
+            zeroFrequency = _zeroFrequency;
         }
 
     private:
@@ -25,6 +29,8 @@ class PitchInput {
         uint32_t value;
         float voltage;
         float frequency;
+
+        float zeroFrequency = 440;
 
 };
 
